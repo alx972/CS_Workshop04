a9da1731049024eff3d680798e3477431c879080
Alex Slyussarev <alex.slyussarev@yandex.ru>
2023-02-25 11:23:09 +0300

    task #2 add complete program

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,1 +1,27 @@
 ﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+int InputInt(string message) // ввод числового значения с консоли 
+{
+    System.Console.Write($"{message} > ");
+    int value;
+    if (int.TryParse(Console.ReadLine(), out value)) // проверка правильности ввода числа
+    {
+        return value;
+    }
+    System.Console.WriteLine("Ошибка: введено не целое число");
+    Environment.Exit(1); // exit code программы при ошибке
+    return 0; // функция возвращает 0, потому что надо что-то возвращать int
+}
+
+int SumDigitsInNumber(int num) // подсчитывает сумму цифр в числе
+{
+    int result = 0;
+    while (num > 0)
+    {
+        result = result + num % 10;
+        num = num / 10;
+    }
+    return result;
+}
+
+int myNumber = InputInt("Введите число");
+System.Console.WriteLine($"Сумма цифр числа {myNumber} составляет {SumDigitsInNumber(myNumber)}");
